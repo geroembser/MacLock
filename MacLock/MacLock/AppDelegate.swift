68f6d7d17bc8eb15c2e946c3564a7c326864ce71
Gero <gero.embser@gmail.com>
2018-07-13 17:38:43 +0200

    init Xcode project

---
+++ MacLock/MacLock/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacLock
+//
+//  Created by Gero Embser on 13.07.18.
+//  Copyright © 2018 Gero Embser. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacLock/MacLock/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacLock
+//
+//  Created by Gero Embser on 13.07.18.
+//  Copyright © 2018 Gero Embser. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
